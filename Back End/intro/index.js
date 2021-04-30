55f1157851a3fb7bf7dc30464328feb5f849604d
idoyudha <idowidya.yudhatama@gmail.com>
2021-04-30 11:23:32 +0700

    Update intro to BackEnd

---
+++ Back End/intro/index.js
@@ -0,0 +1,42 @@
+const http = require('http') // same with import
+const fs = require('fs')
+const url = require('url') // for query
+const PORT = 2000
+
+// JSON:parse => convert buffer format to object format
+// JSON:stringify => convert object format to buffer format
+
+const server = http.createServer((request, response) => {
+    console.log(request.url)
+    if (request.url == '/') {
+        if (request.method == 'GET') {
+            response.writeHead(200, {"Content-Type":"text/html"})
+            response.end("<h2>Hello Papa</h2>")
+        }
+    }
+    else if (request.url.includes('/products')) {
+        let products = fs.readFileSync('./data/products.json')
+        console.log(JSON.parse(products))
+        console.log(url.parse(request.url, true).query.id)
+        if (request.method == 'GET') {
+            response.writeHead(200, {"Content-Type":"text/html"})
+            response.end(products)
+        }
+    }
+    else if (request.url.includes('/users')) {
+        let users = fs.readFileSync('./data/users.json')
+        if (request.method == 'GET') {
+            response.writeHead(200, {"Content-Type":"text/html"})
+            response.end(users)
+        }
+    }
+    else {
+        if (request.method == 'GET') {
+            response.writeHead(400, {"Content-Type":"text/html"})
+            response.end("<h2>Page Not Found</h2>")
+        }
+    }
+
+})
+
+server.listen(PORT, () => console.log(`Server Running at ${PORT}`))

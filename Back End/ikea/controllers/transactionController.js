8db2f13071c11f2afa691af7e6c6dd658b25530d
idoyudha <idowidya.yudhatama@gmail.com>
2021-06-02 13:12:09 +0700

    Add transactio function

--- Back End/ikea/controllers/transactionController.js
+++ Back End/ikea/controllers/transactionController.js
@@ -20,10 +20,10 @@ module.exports = {
     addCart: async (request, response, next) => {
         try {
             let queryInsert = 'INSERT INTO cart SET ?'
-            console.log('INSERT ADD CART', request.body)
+            // console.log('INSERT ADD CART', request.body)
             queryInsert = await dbQuery(queryInsert, request.body)
-            console.log(queryCheck)
-            response.status(200).send({status: "Add Success!", results: queryCheck})
+            // console.log(queryCheck)
+            response.status(200).send({status: "Add Success!", results: queryInsert})
         } 
         catch (error) {
             next(error)
@@ -34,7 +34,7 @@ module.exports = {
         try {
             let queryUpdate = `UPDATE cart SET quantity = ${request.body.quantity} WHERE idcart = ${request.body.idcart}`
             queryUpdate = await dbQuery(queryUpdate, request.body)
-            console.log(queryUpdate)
+            // console.log(queryUpdate)
             response.status(200).send({status: "Add Success!", results: queryUpdate})
         } 
         catch (error) {
@@ -63,5 +63,62 @@ module.exports = {
         catch (error) {
             next(error)
         }
+    },
+
+    moveToTransaction: async (request, response, next) => {
+        try {
+            let queryTransaction = `INSERT INTO db_ikea.transaction (invoice, date, iduser, delivery_cost, total_payment, note, idstatus) 
+            VALUES ('AA1', NOW(), ${request.params.iduser}, 40000, (SELECT SUM(quantity * price) total_payment 
+            FROM db_ikea.cart JOIN db_ikea.product ON cart.idproduct = product.idproduct WHERE iduser = 3), 'NOTE', 6);`
+            let deleteCart = `DELETE FROM cart WHERE iduser = ${request.params.iduser};`
+            let queryDetail = `INSERT INTO transaction_detail (idtransaction, idproduct, idstock, quantity) VALUES ?`
+            
+            // Insert to transaction detail
+            let dataDetail = request.body.map(item => [item.idproduct, item.idcart, item.idstock, item.quantity])
+            queryDetail = await dbQuery(queryDetail, [dataDetail])
+            
+            
+            queryTransaction = await dbQuery(queryTransaction) // Insert to transaction
+            deleteCart = await dbQuery(deleteCart) // Delete cart with iduser
+
+            response.status(200).send({ success: true, message: "Checkout Done!"})      
+        } 
+        catch (error) {
+            next(error)
+        }
+    },
+
+    getTransaction: async (request, response, next) => {
+        try {
+            let getDataTransaction = `SELECT * FROM transaction WHERE iduser = ${request.params.iduser};`
+            let getDataDetail = `SELECT name, type, transaction_detail.quantity, price, (transaction_detail.quantity * price) subtotal FROM transaction_detail JOIN product ON transaction_detail.idproduct = product.idproduct JOIN transaction on transaction.idtransaction = transaction_detail.idtransaction JOIN product_stock ON product_stock.idproduct_stock = transaction_detail.idstock WHERE iduser = ${request.params.iduser}; `
+
+            dataTransaction = await dbQuery(getDataTransaction)
+            // console.log(dataTransaction)
+
+            dataDetail = await dbQuery(getDataDetail)
+            dataTransaction[0].detail = dataDetail
+
+            response.status(200).send(dataTransaction)
+        } 
+        catch (error) {
+            next(error)
+        }
     }
 }
+
+// Sample data for query detail 
+// {
+//     {
+//         "idproduct": 1,
+//         "idcart": 1,
+//         "idstock": 1,
+//         "quantity": 1
+//     },
+//     {
+//         "idproduct": 2,
+//         "idcart": 2,
+//         "idstock": 2,
+//         "quantity": 2
+//     }
+// }
--- Back End/ikea/routes/transactionRoute.js
+++ Back End/ikea/routes/transactionRoute.js
@@ -6,5 +6,7 @@ router.post('/add-cart', transactionController.addCart)
 router.delete('/delete-cart/:id', transactionController.deleteCart)
 router.patch('/update-cart', transactionController.updateCart)
 router.patch('/update-cart-prod', transactionController.updateSameProductInCart)
+router.post('/payment/:iduser', transactionController.moveToTransaction)
+router.get('/payment/:iduser', transactionController.getTransaction)
 
 module.exports = router
